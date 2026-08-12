92033ee720b1af463185f28137f9bca9d8034a3a
Garrett Wink <gwink@exactsciences.com>
2026-08-12 16:16:00 -0500

    initial model

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ def _():
     import torch
     import matplotlib.pyplot as plt
 
-    return plt, torch
+    return mo, plt, torch
 
 
 @app.cell
@@ -56,14 +56,49 @@ def _(X_test, X_train, plt, y_test, y_train):
         plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
 
       # Show the legend
-      plt.legend(prop={"size": 14});
+      plt.legend(prop={"size": 14})
 
     return (plot_predictions,)
 
 
 @app.cell
-def _(plot_predictions):
+def _(plot_predictions, plt):
     plot_predictions()
+    plt.gca()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Building Model
+    """)
+    return
+
+
+@app.cell
+def _(nn, torch):
+    # Create a Linear Regression model class
+    class LinearRegressionModel(torch.nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
+        def __init__(self):
+            super().__init__() 
+            self.weights = nn.Parameter(torch.randn(1, # <- start with random weights (this will get adjusted as the model learns)
+                                                    dtype=torch.float), # <- PyTorch loves float32 by default
+                                       requires_grad=True) # <- can we update this value with gradient descent?)
+
+            self.bias = nn.Parameter(torch.randn(1, # <- start with random bias (this will get adjusted as the model learns)
+                                                dtype=torch.float), # <- PyTorch loves float32 by default
+                                    requires_grad=True) # <- can we update this value with gradient descent?))
+
+        # Forward defines the computation in the model
+        def forward(self, x: torch.Tensor) -> torch.Tensor: # <- "x" is the input data (e.g. training/testing features)
+            return self.weights * x + self.bias # <- this is the linear regression formula (y = m*x + b)
+
+    return
+
+
+@app.cell
+def _():
     return
 
 
