3c8a51bd258ea1df89c0ea8c3ea8031548568832
Garrett Wink <gwink00@gmail.com>
2026-09-09 11:31:30 -0500

    train attempt

--- sports_nn.py
+++ sports_nn.py
@@ -90,7 +90,7 @@ def _(DataLoader, Dataset, decode_image, os, pd, torch, torchvision, v2):
 
     classes = ('Badminton', 'Cricket', 'Tennis', 'Soccer', 'Swimming', 'Karate', 'Wrestling')
         
-    return batch_size, trainloader
+    return batch_size, classes, trainloader
 
 
 @app.cell
@@ -118,6 +118,80 @@ def _(batch_size, torch, torchvision, trainloader):
     imshow(torchvision.utils.make_grid(images))
     # print labels
     print(' '.join(f'{labels[j]:5s}' for j in range(batch_size)))
+    return
+
+
+@app.cell
+def _(classes, torch):
+    import torch.nn as nn
+    import torch.nn.functional as F
+    import torch.optim as optim
+
+    class Net(nn.Module):
+        def __init__(self, input_size=224):
+            super().__init__()
+            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.pool = nn.MaxPool2d(2, 2)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+
+            # compute flattened size dynamically based on actual input size
+            with torch.no_grad():
+                dummy = torch.zeros(1, 3, input_size, input_size)
+                dummy = self.pool(self.conv1(dummy))
+                dummy = self.pool(self.conv2(dummy))
+                flat_size = dummy.numel()
+
+            self.fc1 = nn.Linear(flat_size, 120)
+            self.fc2 = nn.Linear(120, 84)
+            self.fc3 = nn.Linear(84, len(classes))   # num_classes = however many sports categories you have
+
+        def forward(self, x):
+            x = self.pool(F.relu(self.conv1(x)))
+            x = self.pool(F.relu(self.conv2(x)))
+            x = torch.flatten(x, 1)
+            x = F.relu(self.fc1(x))
+            x = F.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+
+    net = Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    return criterion, net, optimizer
+
+
+@app.cell
+def _(criterion, net, optimizer, trainloader):
+    for epoch in range(2):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs1, labels1 = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs1)
+            loss = criterion(outputs, labels1)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
+    return
+
+
+@app.cell
+def _():
     return
 
 
